bdb045bd798b784349fc815d19c6679b89a93558
Andrei Ilinca <andrei.ilinca2405@aut.pub.ro>
2017-11-27 11:51:16 +0200

    styling changes

--- popup/popup.js
+++ popup/popup.js
@@ -13,11 +13,12 @@ document.addEventListener("DOMContentLoaded", () => {
     &includeTags=false
     &api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`;
 
+    console.log('Fetching definition for ' + word);
+
     fetch(url)
         .then(response => {
             if (response.status !== 200) {
-                console.log('Looks like there was a problem. Status Code: ' +
-                    response.status);
+                console.warn('Looks like there was a problem. Status Code: ' + response.status);
                 return;
             }
 
@@ -25,8 +26,12 @@ document.addEventListener("DOMContentLoaded", () => {
             response.json()
                 .then(data => {
                     console.log(data);
-                    console.log("Definition requested is: " + data[0].text);
-                    userInput.innerHTML += '= ' + data[0].text;
+                    if(data.length) {
+                        console.log('Definition requested is: ' + data[0].text);
+                        userInput.innerHTML += ' = ' + data[0].text;
+                    } else {
+                        console.warn('Sorry, it appears that our dictionary did not have a definition for "' + word + '"');
+                    }
 
             });
         })
