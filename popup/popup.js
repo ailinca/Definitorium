acb791c4568bbbca4fcd884b7c6990858e9eaacf
Andrei Ilinca <andrei.ilinca2405@aut.pub.ro>
2017-11-27 11:19:37 +0200

    fetch definition

--- popup/popup.js
+++ popup/popup.js
@@ -5,5 +5,33 @@ document.addEventListener("DOMContentLoaded", () => {
     let backgroundPage = chrome.extension.getBackgroundPage();
     let word = backgroundPage.wordToSearch;
     userInput.innerHTML = word;
+
+    let url = `http://api.wordnik.com:80/v4/word.json/${word}/definitions?
+    limit=1
+    &includeRelated=false
+    &useCanonical=false
+    &includeTags=false
+    &api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`;
+
+    fetch(url)
+        .then(response => {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                return;
+            }
+
+            // Examine the text in the response
+            response.json()
+                .then(data => {
+                    console.log(data);
+                    console.log("Definition requested is: " + data[0].text);
+                    userInput.innerHTML += '= ' + data[0].text;
+
+            });
+        })
+        .catch(err => {
+            console.log('Fetch Error :-S', err);
+        });
 });
 
