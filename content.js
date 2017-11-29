6f8a3ad94801fb57b7bf67cfa0c83490c5728510
Andrei Ilinca <andrei.ilinca2405@aut.pub.ro>
2017-11-29 17:23:06 +0200

    fixed runtime sendMessage issue

--- content.js
+++ content.js
@@ -1,19 +1,32 @@
 console.log("Logging from the content script!");
 
+// receive messages from the background script => currently unused functionality
 const receivedMessage = (message, sender, sendResponse) => {
     console.log(message.text);
+};
+
+// wrap chrome.runtime.sendMessage in a try catch block to avoid background script not running issues
+const sendMessage = (message) => {
+    try {
+        chrome.runtime.sendMessage(message);
+    } catch(e) {
+        if (
+            e.message.match(/Invocation of form runtime\.connect/) &&
+            e.message.match(/doesn't match definition runtime\.connect/)
+        ) {
+            alert('Definitorium has been reloaded. Please refresh the page');
+        } else {
+            throw(e);
+        }
+    }
 };
 
 const wordSelected = () => {
     let selectedText = window.getSelection().toString().trim();
     console.log(selectedText);
-    if (selectedText.length > 0) {
-        let message = {
-            text: selectedText
-        };
-        chrome.runtime.sendMessage(message);
-    }
+    selectedText.length > 0 ? sendMessage({text: selectedText}) : console.log('Empty string selected!');
 };
+
 
 // receive message from background script
 chrome.runtime.onMessage.addListener(receivedMessage);
